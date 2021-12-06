ab42f0c06a0097db927274bfd3cd7853f4c71f39
Mizuki_1950641 <70007256+Aeroraven@users.noreply.github.com>
2021-12-06 15:49:06 +0800

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path=require('path');
+module.exports={
+    entry:'./src/index.js',
+    output:{
+        path:path.resolve(__dirname,'./dist/'),
+        filename:'bundle.js'
+    },
+    module:{},
+    plugins:[],
+    mode: 'development',
+    devServer:{
+        static: './dist',
+        compress: true,
+        port:2333
+    }
+}
